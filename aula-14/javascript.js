a591c04b7565f33e3e305f08d9396b900d3735a5
Sarah Keyse Soeiro <sarahkeyse.soeiro@gmail.com>
2026-05-13 22:34:12 -0300

    Add files via upload

---
+++ aula-14/javascript.js
@@ -0,0 +1,22 @@
+var x = 10;
+if (x>5) {
+    console.log ("x is greater thar 5"); 'x is greater than 5'
+} else if (x<5) {
+    console.log ("x is less than 5");
+} else {
+    console.log("x is equal to 5");
+}
+
+for (let i = 0; i < 10; i++) {
+    if (i % 2 === 0) {
+    console.log(i);
+};
+}
+
+for (let i = 0; i <10 ; i += 2) {
+    console.log(i);
+};
+
+for (let i = 1; i<10; i +=2 ){
+    console.log(i);
+};
